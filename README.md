df80f3b9b149fb9729170b0df3a5c36396fe45c9
Antonia Gallardo <antonia.gallardo.gomez@gmail.com>
2020-05-07 17:12:00 +0200

    demo ionic grupo 3 - Jordi Salvador

---
+++ README.md
@@ -0,0 +1,10 @@
+# Endpoint REST 
+https://oghuxxw1e6.execute-api.us-east-1.amazonaws.com/dev
+
+# Base url tutorial
+https://edupala.com/how-to-add-leaflet-map-in-ionic/
+
+# Bug ya que no importa los iconos de leaflet
+import 'leaflet/dist/images/marker-icon-2x.png';
+
+
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Map, tileLayer, marker} from 'leaflet';
+import 'leaflet/dist/images/marker-icon-2x.png';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-tab3',
@@ -6,7 +11,37 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+  map: Map;
+  restaurants: Observable<any>;
 
-  constructor() {}
+  constructor(public httpClient: HttpClient) {}
+
+  ionViewDidEnter() {
+    this.restaurants = this.httpClient.get('https://oghuxxw1e6.execute-api.us-east-1.amazonaws.com/dev');
+    this.restaurants
+    .subscribe(data => {
+      console.log('my data: ', data);
+      this.initMap(data);
+    });
+
+  }
+
+  ionViewWillLeave() {
+    this.map.remove();
+  }
+
+  initMap(restaurants) {
+    const map = new Map('mapId4').setView([33.6396965, -84.4304574], 23);
+
+    tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    restaurants.forEach((restaurant) => {
+      marker([restaurant.position.lat, restaurant.position.lgn])
+      .bindPopup(`<b>${restaurant.title}</b>`, { autoClose: false })
+      .addTo(map).openPopup();
+    });
+  }
 
 }
